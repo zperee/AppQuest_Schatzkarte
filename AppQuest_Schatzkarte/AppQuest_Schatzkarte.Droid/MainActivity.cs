be4c7d02a17c25086312dbf26455c326956feb10
zperee <elia.perenzin@bluewin.ch>
2016-10-26 15:08:13 +0200

    File save added

--- AppQuest_Schatzkarte/AppQuest_Schatzkarte.Droid/MainActivity.cs
+++ AppQuest_Schatzkarte/AppQuest_Schatzkarte.Droid/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using Acr.UserDialogs;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Plugin.Geolocator;
@@ -24,7 +25,7 @@ namespace AppQuest_Schatzkarte.Droid
 
             FormsMaps.Init(this, bundle);
 
-            
+            UserDialogs.Init(() => (Activity)Forms.Context);
 
             LoadApplication(new App());
         }
--- AppQuest_Schatzkarte/AppQuest_Schatzkarte/Pages/HomePage.xaml.cs
+++ AppQuest_Schatzkarte/AppQuest_Schatzkarte/Pages/HomePage.xaml.cs
@@ -5,91 +5,146 @@ using Xamarin.Forms.Maps;
 using Plugin.Geolocator;
 using Plugin.Geolocator.Abstractions;
 using Position = Xamarin.Forms.Maps.Position;
+using PCLStorage;
+using Acr.UserDialogs;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace AppQuest_Schatzkarte.Pages
 {
-    public partial class HomePage : ContentPage
-    {
-        private Plugin.Geolocator.Abstractions.Position _position;
-        private Map _map;
-        private bool _sucess;
-        public HomePage()
-        {
-            _map = new Map(MapSpan.FromCenterAndRadius(new Position(47.2236, 8.8180), Distance.FromMeters(500)));
-            Content = _map;
-            InitializeComponent();
-        }
+	public partial class HomePage : ContentPage
+	{
+		private const string Folder = "AppQuest_Schatzkarte";
+		private const string File = "Data.json";
+		private IFile _localFile;
+		private IFolder _localFolder;
+		private IFolder _rootFolder;
+		private Plugin.Geolocator.Abstractions.Position _position;
+		private bool _GPSAvailable;
+		private Map _map;
+		private bool _isBusy;
 
-        public void OnLocateClicked(object sender, EventArgs e)
-        {
-            GetCurrentLocation();
-            if (_sucess)
-            {
-                var pin = new Pin
-                {
-                    Type = PinType.Generic,
-                    Label = "",
-                    Position = new Position(_position.Latitude, _position.Longitude)
-                };
-                _map.Pins.Add(pin);
-                _sucess = false;
-            }            
-        }
+		public HomePage()
+		{
+			_map = new Map(MapSpan.FromCenterAndRadius(new Position(47.2236, 8.8180), Distance.FromMeters(500)));
+			Content = _map;
+			InitializeComponent();
+		}
 
-        private void OnAddNewPinClicked(object sender, EventArgs e)
-        {
-            if (_position == null)
-            {
-                GetCurrentLocation();
-                if (_sucess)
-                {
-                    var pin = new Pin
-                    {
-                        Type = PinType.SavedPin,
-                        Label = "",
-                        Position = new Position(_position.Latitude, _position.Longitude)
-                    };
-                    _map.Pins.Add(pin);
-                    _position = null;
-                }
-                _sucess = false;
-            }
-            else
-            {
-                var pin = new Pin
-                {
-                    Type = PinType.SavedPin,
-                    Label = "",
-                    Position = new Position(_position.Latitude, _position.Longitude)
-                };
-                _map.Pins.Add(pin);
-                _position = null;
-            }
-            
-        }
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			_rootFolder = FileSystem.Current.LocalStorage;
+			_localFolder = await _rootFolder.CreateFolderAsync(Folder, CreationCollisionOption.OpenIfExists);
+			_localFile = await _localFolder.CreateFileAsync(File, CreationCollisionOption.OpenIfExists);
 
-        private async void GetCurrentLocation()
-        {
-            var locator = CrossGeolocator.Current;
-            locator.DesiredAccuracy = 5;
-            
-            try
-            {
-                _position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
-                _sucess = true;
-            }
-            catch (Exception)
-            {
-                _position = null;
-                _sucess = false;
-            }
+			var result = "";
+			result = await _localFile.ReadAllTextAsync();
 
-            if (_position == null)
-            {
-                await DisplayAlert("Fehler", "GPS nicht verfügbar", "OK");
-                
-            }
-            
-        }
-    }
+			if (result.Length > 0)
+			{
+				FillPins(result);
+			}
+		}
+
+		public void FillPins(string file)
+		{
+			var json = JsonConvert.DeserializeObject<IEnumerable<Pin>>(file);
+			foreach (var item in json)
+			{
+				_map.Pins.Add(item);
+			}
+		}
+
+		public async void OnLocateClicked(object sender, EventArgs e)
+		{
+			await GetCurrentLocation();
+			if (_GPSAvailable)
+			{
+				var pin = new Pin
+				{
+					Type = PinType.Generic,
+					Label = "",
+					Position = new Position(_position.Latitude, _position.Longitude)
+				};
+				_map.Pins.Add(pin);
+			}
+			else {
+				await DisplayAlert("Fehler", "GPS nicht verfügbar", "OK");
+			}
+		}
+
+		private async void OnAddNewPinClicked(object sender, EventArgs e)
+		{
+			if (_position == null)
+			{
+				await GetCurrentLocation();
+			}
+
+			if (_GPSAvailable)
+			{
+				var text = NamePinInput();
+				var pin = new Pin
+				{
+					Type = PinType.Generic,
+					Label = text,
+					Position = new Position(_position.Latitude, _position.Longitude)
+				};
+				_map.Pins.Add(pin);
+				await SaveFile();
+
+				_position = null;
+			}
+			else {
+				await DisplayAlert("Fehler", "GPS nicht verfügbar", "OK");
+			}
+		}
+
+		private async Task SaveFile()
+		{
+			var json = JsonConvert.SerializeObject(_map.Pins);
+			await _localFile.WriteAllTextAsync(json);
+		}
+
+		private string NamePinInput()
+		{
+			var input = "tet";
+				UserDialogs.Instance.Prompt(new PromptConfig{
+				Title = "Pin Name",
+				InputType = InputType.Default, 
+				OkText = "Erstellen",
+				CancelText = "Abbrechen",
+				OnAction = result =>
+				{
+					if (!result.Ok) return;
+					input = result.Text;
+				}});
+			return input;
+		}
+
+		private async Task GetCurrentLocation()
+		{
+			var locator = CrossGeolocator.Current;
+			locator.DesiredAccuracy = 5;
+
+			try
+			{
+				_isBusy = true;
+				_position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+				_isBusy = false;
+				_GPSAvailable = true;
+
+				if (_position == null)
+				{
+					_GPSAvailable = false;
+				}
+			}
+			catch (Exception)
+			{
+				_position = null;
+				_GPSAvailable = false;
+			}
+
+		}
+	}
 }
